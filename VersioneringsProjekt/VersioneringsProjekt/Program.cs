664894ded66be97cb7a2c4338db867755abce619
DESKTOP-9R3EA4E\Tec <lasse.bjoern@hotmail.dk>
2017-12-06 13:58:39 +0100

    Initialise project

---
+++ VersioneringsProjekt/VersioneringsProjekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VersioneringsProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 06-12-2017 af Lasse
+            Console.WriteLine("It works");
+
+            Console.ReadKey();
+        }
+    }
+}

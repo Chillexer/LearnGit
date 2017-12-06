4571bed6aaaf37242b4fb3091da589083ffeba65
DESKTOP-9R3EA4E\Tec <lasse.bjoern@hotmail.dk>
2017-12-06 14:06:13 +0100

    Jeg har lavet nolge ændringer i main

--- VersioneringsProjekt/VersioneringsProjekt/Program.cs
+++ VersioneringsProjekt/VersioneringsProjekt/Program.cs
@@ -8,6 +8,8 @@ namespace VersioneringsProjekt
         {
             // 06-12-2017 af Lasse
             Console.WriteLine("It works");
+            string test = "Det her er en test";
+            Console.WriteLine(test);
 
             Console.ReadKey();
         }
